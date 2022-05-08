3a281bc890acf560e445c594053c654c42923eff
zengtai <zengtai@gmail.com>
2022-05-08 19:35:53 +0800

    修复分类游戏数量显示错误

--- src/templates/category.js
+++ src/templates/category.js
@@ -10,14 +10,15 @@ export default function CategoryTemplate({
   console.log(categories)
   console.log(games)
   const allGames = games.filter(game => game.category.name === category.name)
+  console.log(allGames)
 
   return (
     <Layout navItems={categories}>
       <Seo title={category.name} />
       <div className="container mx-auto">
         <h2 className="px-4 text-lg font-bold">
-          All {category.name} {games.length > 1 ? `Games` : `Game`} (
-          {games.length})
+          All {category.name} {allGames.length > 1 ? `Games` : `Game`} (
+          {allGames.length})
         </h2>
         <List items={allGames} type="grid" />
       </div>
