328f04b1fcd193e42a1df47deebebd233eec69e0
zengtai <zengtai@gmail.com>
2022-05-08 21:19:38 +0800

    添加Adsense

---
+++ src/lib/constants.js
@@ -0,0 +1,7 @@
+export const GAME_PATH = `https://uptapapi.uptap.com/h5Game/?type=Game&platform=baxiang_main&appid=`
+export const ADS_ID = `ca-pub-9209477879340784`
+export const GA_ID = `UA-183890722-16`
+
+export const HOME_ADS_ID = `1977361307`
+export const CAT_ADS_ID = `1977361307`
+export const DETAIL_ADS_ID = `1977361307`
