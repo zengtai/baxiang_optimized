0d61a14c8b65fe7a589a2f997f0c1a8c408a9efb
Tai Zeng <zengtai@gmail.com>
2022-05-12 02:10:11 +0800

    增加分辨率区别

--- src/components/image.js
+++ src/components/image.js
@@ -4,12 +4,19 @@ export default function Image({ className, src, alt, width, height, lazy }) {
   return (
     <>
       <picture className={className}>
-        <source type="image/avif" srcSet={`/avif/${src}.avif`} />
-        <source type="image/webp" srcSet={`/webp/${src}.webp`} />
+        <source
+          type="image/avif"
+          srcSet={`/avif/${src}.avif 1x, /avif/${src}.2x.avif 2x,`}
+        />
+        <source
+          type="image/webp"
+          srcSet={`/webp/${src}.webp 1x, /webp/${src}.2x.webp 2x,`}
+        />
         <img
           width={width}
           height={height}
-          src={`/games/${src}.png`}
+          src={`/mozjpeg/${src}.jpg`}
+          srcSet={`/mozjpeg/${src}.jpg 1x, /mozjpeg/${src}.2x.jpg 2x`}
           alt={alt}
           loading={lazy ? `lazy` : `eager`}
           decoding="async"
