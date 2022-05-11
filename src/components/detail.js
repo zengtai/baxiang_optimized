f258b97424e813abd2a573e6ec00df9b5bddc9dc
zengtai <zengtai@gmail.com>
2022-05-11 10:33:26 +0800

    延迟Banner加载

--- src/components/detail.js
+++ src/components/detail.js
@@ -1,7 +1,7 @@
 import * as React from "react"
-import { Link, useStaticQuery, graphql } from "gatsby"
+import { Link } from "gatsby"
 import { GAME_PATH } from "../lib/constants"
-import { GatsbyImage } from "gatsby-plugin-image"
+// import { GatsbyImage } from "gatsby-plugin-image"
 
 export default function Detail({ game }) {
   return (
--- src/templates/home.js
+++ src/templates/home.js
@@ -1,12 +1,19 @@
-import * as React from "react"
+import React, { lazy, Suspense } from "react"
 // import { Link, useStaticQuery, graphql } from "gatsby"
 // import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import List from "../components/list"
-import Banner from "../components/banner"
+
 import { FEATURED_GAMES, ADS_SLOT_ID } from "../lib/constants"
+
+const Banner = lazy(() => import("../components/banner"))
+const renderLoader = () => (
+  <div className="AdContainer banner bg-black/10 text-center opacity-20">
+    Loading...
+  </div>
+)
 
 const Home = ({ pageContext: data }) => {
   // console.log(data.categories)
@@ -21,13 +28,14 @@ const Home = ({ pageContext: data }) => {
   return (
     <Layout navItems={categories}>
       <Seo title="Home" />
-      <Banner
-        className={`banner`}
-        style={{ display: "block" }}
-        slot={ADS_SLOT_ID.home}
-        responsive="false"
-      />
-
+      <Suspense fallback={renderLoader()}>
+        <Banner
+          className={`banner`}
+          style={{ display: "block" }}
+          slot={ADS_SLOT_ID.home}
+          responsive="false"
+        />
+      </Suspense>
       <div className="container mx-auto">
         <h2 className="px-4 text-center text-lg font-bold">
           All Games ({allGames.length})
@@ -35,13 +43,14 @@ const Home = ({ pageContext: data }) => {
         <List items={topGames} type="grid" focus />
         <List items={normalGames.slice(0, 6)} type="card" />
         <List items={normalGames.slice(6, 42)} type="grid" lazy={true} />
-        <Banner
-          className={`banner rectangle mb-6`}
-          style={{ display: "block" }}
-          slot={ADS_SLOT_ID.home}
-          format=""
-          responsive="false"
-        />
+        <Suspense fallback={renderLoader()}>
+          <Banner
+            className={`banner`}
+            style={{ display: "block" }}
+            slot={ADS_SLOT_ID.home}
+            responsive="false"
+          />
+        </Suspense>
         <List items={normalGames.slice(42, 48)} type="card" lazy={true} />
         <List items={normalGames.slice(48, 72)} type="grid" lazy={true} />
         <List items={normalGames.slice(72)} type="card" lazy={true} />
