cf532b12f4f228a3cd5b66f11efd9626b932dc2c
zengtai <zengtai@gmail.com>
2022-05-12 22:00:12 +0800

    导出路径设置

--- src/components/image.js
+++ src/components/image.js
@@ -1,24 +1,24 @@
 import React from "react"
 
 export default function Image({ className, src, alt, width, height, lazy }) {
-  const path = ``
-  // const path = `/webs/baxiang/baxiang_main_new_4`
+  const path = `/`
+  // const path = `/webs/baxiang/baxiang_main_new_4/`
   return (
     <>
       <picture className={className ? className : ``}>
         <source
           type="image/avif"
-          srcSet={`${path}/avif/${src}.2x.avif 1x, ${path}/avif/${src}.2x.avif 2x`}
+          srcSet={`${path}avif/${src}.2x.avif 1x, ${path}avif/${src}.2x.avif 2x`}
         />
         <source
           type="image/webp"
-          srcSet={`${path}/webp/${src}.2x.webp 1x, ${path}/webp/${src}.2x.webp 2x`}
+          srcSet={`${path}webp/${src}.2x.webp 1x, ${path}webp/${src}.2x.webp 2x`}
         />
         <img
           width={width}
           height={height}
           className={`bg-black/5`}
-          src={`${path}/mozjpeg/${src}.2x.jpg`}
+          src={`${path}mozjpeg/${src}.2x.jpg`}
           alt={alt}
           loading={lazy ? `lazy` : `eager`}
           decoding="async"
