c403a4d8efcabe63eaf166264f8357a191d2bcea
Tai Zeng <zengtai@gmail.com>
2022-05-14 08:44:45 +0800

    补回低分辨率尺寸图片

--- src/components/image.js
+++ src/components/image.js
@@ -9,16 +9,17 @@ export default function Image({ className, src, alt, width, height, lazy }) {
       <picture className={className}>
         <source
           type="image/avif"
-          srcSet={`${path}/avif/${src}.avif 1x, ${path}/avif/${src}.avif 2x`}
+          srcSet={`${path}/avif/${src}.avif 1x, ${path}/avif/2x/${src}.avif 2x`}
         />
         <source
           type="image/webp"
-          srcSet={`${path}/webp/${src}.webp 1x, ${path}/webp/${src}.webp 2x`}
+          srcSet={`${path}/webp/${src}.webp 1x, ${path}/webp/2x/${src}.webp 2x`}
         />
         <img
           width={width}
           height={height}
           src={`${path}/mozjpeg/${src}.jpg`}
+          srcSet={`${path}/webp/${src}.jpg 1x, ${path}/webp/2x/${src}.jpg 2x`}
           alt={alt}
           loading={lazy ? `lazy` : `eager`}
           decoding="async"
