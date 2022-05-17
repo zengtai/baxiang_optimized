8d3fd36659497f67e089db96b39c928cc6f4c88e
zengtai <zengtai@gmail.com>
2022-05-17 18:32:13 +0800

    暂存

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,22 +11,36 @@ module.exports = {
     author: `@SuperEasyGame`,
     siteUrl: `https://www.supereasygame.com/`,
   },
-  pathPrefix: `/webs/baxiang/baxiang_main_new_5`, // 导出路径
+  pathPrefix: `/webs/youle`, // 导出路径
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: ["G-DDF82DXW7Z"],
-      },
-      pluginConfig: {
-        // Puts tracking script in the head instead of the body
-        head: false,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        trackingIds: [
+          "G-DDF82DXW7Z", // Google Analytics / GA
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+          // Defaults to https://www.googletagmanager.com
+          origin: "YOUR_SELF_HOSTED_ORIGIN",
+        },
       },
     },
     `gatsby-plugin-image`,
--- src/components/image.js
+++ src/components/image.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { EXPORT_PATH } from "../lib/constants"
+// import { EXPORT_PATH } from "../lib/constants"
 
 export default function Image({ className, src, alt, width, height, lazy }) {
   // const path = EXPORT_PATH.length ? EXPORT_PATH : ``
