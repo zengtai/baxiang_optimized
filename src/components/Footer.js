dd1f084a26204699f1c9815dc9e09e9680f96227
zengtai <zengtai@gmail.com>
2022-05-24 02:38:43 +0800

    底部样式修复

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,11 +12,11 @@ export default function Footer() {
     }
   `)
   return (
-    <footer className="mt-4 flex flex-col justify-center bg-emerald-700/80">
-      <nav className="block bg-emerald-800 p-2">
+    <footer className="mt-4">
+      <nav className="flex justify-center bg-black/10 p-2 text-white/50 ">
         <Link to="/privacy-policy">Privacy Policy</Link>
       </nav>
-      <div className="container text-center text-sm opacity-40">
+      <div className="py-6 text-center text-sm opacity-40">
         Copyright © {new Date().getFullYear()} &middot;{" "}
         {data.site.siteMetadata.title}
       </div>
