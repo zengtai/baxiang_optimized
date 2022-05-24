5afe3ccd2a4bfe4ac8e6f3fe3b3be6ec1eedcba5
zengtai <zengtai@gmail.com>
2022-05-25 01:48:45 +0800

    增加协议

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,8 +13,9 @@ export default function Footer() {
   `)
   return (
     <footer className="mt-4">
-      <nav className="flex justify-center bg-black/10 p-2 text-white/50 ">
+      <nav className="flex justify-center gap-4 bg-black/10 p-2 text-white/50 ">
         <Link to="/privacy-policy">Privacy Policy</Link>
+        <Link to="/terms-of-use">Terms of Use</Link>
       </nav>
       <div className="py-6 text-center text-sm opacity-40">
         Copyright © {new Date().getFullYear()} &middot;{" "}
